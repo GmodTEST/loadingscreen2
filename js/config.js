5534c4ed9de35e88dd8cc2403f3ed033ca7c2c85
GithubTEST <emirakyldz1081@gmail.com>
2026-04-17 17:59:06 +0200

    update.images

--- js/config.js
+++ js/config.js
@@ -58,13 +58,13 @@ Config.announcementLength = 3000;
  * Görseller buraya eklenen sırayla döner.
  */
 Config.backgrounds = [
-// "legacyyilbasi.png",
-//  "legacyyilbasi2.png",
-  
-"default1.jpg",
-"default4.jpg",
-"default2.jpg",
-"default3.jpg"
+  // "legacyyilbasi.png",
+  //  "legacyyilbasi2.png",
+
+  "default1.jpg",
+  "default4.jpg",
+  "default2.jpg",
+  "default3.jpg"
 ];
 // Kaç saniyede bir arka plan değişsin?
 Config.backgroundInterval = 5; // saniye
@@ -74,25 +74,26 @@ Config.backgroundInterval = 5; // saniye
  * Sağ tarafta gösterilecek yetkili listesi
  */
 Config.staff = [
- { rank: "LEGACY", name: "Kly" },
- { rank: "LEGACY", name: "Valeri" },
- { rank: "LEGACY", name: "Goktug" },
- { rank: "LEGACY", name: "Muzisyenes" },
- { rank: "Sunucu Yönetimi", name: "Aren" },
- { rank: "Sunucu Yönetimi", name: "wAve" },
- { rank: "Yetkili Sorumlusu", name: "codibaba1numara" },
- { rank: "Admin", name: "InFlames" },
- { rank: "Admin", name: "Keegan" },
- { rank: "Admin", name: "pirasa" },
- { rank: "Admin", name: "Orpheus" },
- { rank: "Kıdemli Moderatör", name: "Raft" },
- { rank: "Kıdemli Moderatör", name: "Integra" },
- { rank: "Kıdemli Moderatör", name: "4pollon" },
- { rank: "Kıdemli Moderatör", name: "Eekmek" },
- { rank: "Moderatör", name: "Starspro" },
- { rank: "Moderatör", name: "Grodde" },
- { rank: "Moderatör", name: "MLHC" },
- { rank: "Moderatör", name: "Ayes" }
+  { rank: "LEGACY", name: "Kly" },
+  { rank: "LEGACY", name: "Valeri" },
+  { rank: "LEGACY", name: "Goktug" },
+  { rank: "LEGACY", name: "Muzisyenes" },
+  { rank: "Sunucu Yönetimi", name: "Aren" },
+  { rank: "Sunucu Yönetimi", name: "wAve" },
+  { rank: "Yetkili Sorumlusu", name: "codibaba1numara" },
+  { rank: "Admin", name: "InFlames" },
+  { rank: "Admin", name: "Keegan" },
+  { rank: "Admin", name: "pirasa" },
+  { rank: "Admin", name: "Orpheus" },
+  { rank: "Admin", name: "4pollon" },
+  { rank: "Kıdemli Moderatör", name: "Raft" },
+  { rank: "Kıdemli Moderatör", name: "Integra" },
+  { rank: "Kıdemli Moderatör", name: "Eekmek" },
+  { rank: "Moderatör", name: "Starspro" },
+  { rank: "Moderatör", name: "Grodde" },
+  { rank: "Moderatör", name: "MLHC" },
+  { rank: "Rehber", name: ".maviii" },
+  { rank: "Rehber", name: "ibowiee" }
 ];
 
 /**
@@ -127,7 +128,6 @@ Config.tips = [
   "İksir ustaları sabır ve hassasiyetle çalışır.",
   "Ölülere değil, yaşayanlara acıyın. Her şeyden önce sevgisiz yaşayanlara..",
   "Uçan süpürge üreticileri büyük paralar kazanmakatdır.",
-  "Merak günah değildir, onların peşinden koşabiliriz. Ancak her ihtimale karşı dikkatli olmakta fayda var..",
   "Büyücülük evreninde bir zihnibend veya zihnifend konusunda yetekenkli büyücülere rastlamak pek yaygın bir durum değildir.",
   "Tılsım ustaları ustaları savunmayı saldırıdan üstün tutar.",
   "Yasaklı kitaplara ilgi duyanlar içindeki tehlikeyi bilse de okumaktan vazgeçmez.",

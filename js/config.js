803e2c61048f5ec41fbd1e522bc84969444880f2
GmodTEST <emirakyldz1081@gmail.com>
2026-04-17 18:10:42 +0200

    testt

--- js/config.js
+++ js/config.js
@@ -58,13 +58,12 @@ Config.announcementLength = 3000;
  * Görseller buraya eklenen sırayla döner.
  */
 Config.backgrounds = [
-  // "legacyyilbasi.png",
-  //  "legacyyilbasi2.png",
-
+/**
   "default1.jpg",
   "default4.jpg",
   "default2.jpg",
   "default3.jpg"
+   */
 ];
 // Kaç saniyede bir arka plan değişsin?
 Config.backgroundInterval = 5; // saniye
